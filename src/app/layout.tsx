ae21bb32a152711e846f368a5da2447667b54716
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-06-30 23:15:08 +0100

    Add property modal start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import AddModal from './components/AddModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <AddModal />
       <body className={inter.className}>{children}</body>
     </html>
   )
