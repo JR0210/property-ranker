e31e518dfe0619efde670db5e6809c4e7e53d32c
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-06-30 12:20:19 +0100

    Changed ctrl v if touch screen

--- src/app/components/PropertyAdd.tsx
+++ src/app/components/PropertyAdd.tsx
@@ -1,38 +1,48 @@
 "use client";
 
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
+import { useTouchscreenDetection } from "@/hooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 
 export default function PropertyAdd() {
+  const isTouchscreen = useTouchscreenDetection();
   const [propertyUrls, setPropertyUrls] = useState<string[]>([]);
   return (
     <div className="flex flex-col gap-4 text-center items-center">
-      <h2 className="text-2xl bold">To add properties:</h2>
-      <div className="flex items-center text-lg w-fit relative">
-        <kbd className="kbd">CTRL</kbd>&nbsp;+&nbsp;<kbd className="kbd">V</kbd>
-        <div className="mb-auto ml-2 tooltip tooltip-right" data-tip="Expects a CSV (comma separated list) of Rightmove URLs">
-          <FontAwesomeIcon icon={faQuestionCircle} className="" />
+      <h2 className="text-xl bold">To add properties:</h2>
+      {!isTouchscreen ? (
+        <div className="flex items-center text-lg w-fit relative">
+          <kbd className="kbd w-16">CTRL</kbd>&nbsp;+&nbsp;
+          <kbd className="kbd w-16">V</kbd>
+          <div
+            className="mb-auto ml-2 tooltip tooltip-right absolute -right-6 top-0"
+            data-tip="Expects a CSV (comma separated list) of Rightmove URLs"
+          >
+            <FontAwesomeIcon icon={faQuestionCircle} />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center text-lg w-fit relative">
+            <button className="btn">Paste</button>
+            <div
+              className="mb-auto ml-2 tooltip tooltip-bottom absolute -right-6 top-0"
+              data-tip="Expects a CSV (comma separated list) of Rightmove URLs"
+            >
+              <FontAwesomeIcon icon={faQuestionCircle} color="" />
+            </div>
+          </div>
+        </div>
+      )}
       <span className="text-accent text-lg font-extrabold">OR</span>
       <div
         tabIndex={0}
         className="collapse collapse-arrow border border-base-300 bg-base-200 text-left"
       >
-        <div className="collapse-title text-lg font-medium">
+        <label htmlFor="property_add_modal" className="btn text-lg font-medium">
           Add Property Links
-        </div>
-        <div className="collapse-content">
-          <div className="flex flex-col gap-4">
-            <input
-              type="text"
-              className="input input-bordered"
-              placeholder="https://www.rightmove.co.uk/properties/108123456"
-              onFocus={(e) => e.stopPropagation()}
-            />
-          </div>
-        </div>
+        </label>
       </div>
     </div>
   );
