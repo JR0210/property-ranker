0c27a3a1b63f7dde5ffc077d5816cc732170e540
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-07-02 21:10:13 +0100

    Fixed issues with RowInput

--- src/components/RowInput.tsx
+++ src/components/RowInput.tsx
@@ -1,46 +1,17 @@
-"use client";
+interface InputRowType {
+  value: string;
+  onChange: (value: string) => void;
+  valid: boolean;
+};
 
-import { useState, useEffect } from 'react';
-
-function validateUrl(value: string) {
-  try {
-    const url = new URL(value);
-    console.log(url, 'url')
-    return url.hostname.includes('rightmove.co.uk');
-  } catch (_) {
-    return false;  
-  }
-}
-
-export default function CustomInput({ onInputChange }: any) {
-  const [value, setValue] = useState('');
-  const [touched, setTouched] = useState(false);
-  const [valid, setValid] = useState(true);
-
-  useEffect(() => {
-    if (touched) {
-      setValid(validateUrl(value));
-    }
-  }, [value, touched]);
-
-  const handleBlur = () => {
-    setTouched(true);
-    setValid(validateUrl(value));
-  };
-
-  const handleChange = (event: any) => {
-    setValue(event.target.value);
-    // onInputChange(event.target.value);
-  };
-
+export default function CustomInput({ value, onChange, valid }: InputRowType) {
   return (
     <input
       type="url"
       className={`input input-bordered w-full ${valid ? 'input-accent' : 'input-error'}`}
       placeholder="Rightmove URL"
       value={value}
-      onChange={handleChange}
-      onBlur={handleBlur}
+      onChange={e => onChange(e.target.value)}
     />
   );
 };
