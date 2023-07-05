349c38bd57af2ab15003ee796a94cdd542bb3354
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-07-06 00:49:11 +0100

    Commented out TODOs, started API calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
 "use client";
 
-import { useState, useContext } from "react";
+import { useState, useEffect } from "react";
 import { getPostcodeRatingArea } from "@/utils";
 import { Roboto_Serif } from "next/font/google";
 import PropertyAdd from "@/components/PropertyAdd";
 import PropertiesContext from "@/PropertiesContext";
 
 const robotoSerif = Roboto_Serif({ subsets: ["latin"] });
-// TODO APIs/Endpoints to use
-// https://google.com/maps/search/WC2N+5DU
-// https://api.postcodes.io/postcodes/WC2N%205DU
-// https://uk.api.just-eat.io/docs#operation/restaurantsBypostcodePostcodeGet - deprecated (no new call?)
-// https://data.police.uk/docs/method/crime-street/
-// https://data.police.uk/docs/method/stops-street/
-// Get highest internet speed from rightmove page model
+// // https://google.com/maps/search/WC2N+5DU
+// // https://uk.api.just-eat.io/docs#operation/restaurantsBypostcodePostcodeGet - deprecated (no new call?)
+// // https://data.police.uk/docs/method/crime-street/
+// // https://data.police.uk/docs/method/stops-street/
+// // Get highest internet speed from rightmove page model -- ISSUE: Hits captcha
+// // ^ Maybe use https://www.broadbandchoices.co.uk/packages/new-grid/default?location={outcode}+{incode}#/?sortBy=Speed and add button to card
 // // Create hardcoded object & Map of insurance rating areas for postcodes
 
 export default function Home() {
   const [propertyUrls, setPropertyUrls] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log(propertyUrls, 'propertyUrls')
+    async function makeCall() {
+      const res = await fetch("/api/properties", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ url: propertyUrls[0] }),
+        });
+        console.log(res, 'res')
+        const data = await res.json();
+        console.log(data, 'data')
+    }
+
+    if (propertyUrls.length > 0) makeCall();
+  }, [propertyUrls])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-8">
