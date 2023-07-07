97f4806b39f61e2aee588ad8b616aa13198e72b8
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-07-07 02:52:57 +0100

    Added skeleton bg image extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,36 @@ module.exports = {
       themes: ["cupcake", "dark", "cmyk"],
     },
     extend: {
-      backgroundImage: {
+      backgroundImage: (theme) => ({
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "skeleton-gradient": `linear-gradient(
+          to right,
+          #a6adba 2%,
+          #bbbfc7 18%,
+          #a6adba 33%
+        )`,
+        "light-skeleton-gradient": `linear-gradient(
+          to right,
+          #1f2937 2%,
+          #32455c 18%,
+          #1f2937 33%
+        )`,
+      }),
+      keyframes: {
+        "left-to-right": {
+          "0%": { backgroundPosition: "-650px 0" },
+          "100%": { backgroundPosition: "650px 0" },
+        },
+      },
+      animation: {
+        "left-to-right": "left-to-right 3s linear infinite forwards",
+      },
+    },
+    variants: {
+      extend: {
+        backgroundImage: ["dark"],
       },
     },
   },
