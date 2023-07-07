2463c351869fcd2dc788769526f5e3b0b157e119
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-07-07 02:54:16 +0100

    Started adding loading/skeleton

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
+import Skeleton from "./Skeleton";
 
-export default function PropertyCard({ propertyData }: any) {
-  console.log(propertyData, "propertyData");
+export default function PropertyCard({ propertyData, skeleton }: any) {
   const { property = {} } = propertyData;
-  const { displayImage = '', address = {} } = property;
-  console.log(displayImage, "displayImage");
+  const { displayImage = "", address = {} } = property;
   return (
-    <div className="card w-96 bg-neutral shadow-xl">
-      <figure>
-        <Image
-          src={displayImage}
-          alt={address.road}
-          width={1000}
-          height={1000}
-        />
+    <div className="card w-96 shadow-xl">
+      <figure className="flex flex-center bg-base-200 h-72 relative text-">
+        {skeleton ? (
+          <div className="loading loading-lg"></div>
+        ) : (
+          <Image src={displayImage} alt={address.road} fill />
+        )}
       </figure>
       <div className="card-body">
         <h2 className="card-title">Property</h2>
+        <Skeleton />
         <p>Property Description</p>
         <div className="card-actions justify-end">
           <button className="btn btn-accent">View</button>
