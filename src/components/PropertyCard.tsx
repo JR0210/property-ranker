3ccea4d5468fe00c693ad4726143bf73fb50a9f9
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-07-08 00:09:03 +0100

    Added loading check for skeleton on card

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -4,6 +4,26 @@ import Skeleton from "./Skeleton";
 export default function PropertyCard({ propertyData, skeleton }: any) {
   const { property = {} } = propertyData;
   const { displayImage = "", address = {} } = property;
+
+  const renderBody = () => {
+    if (skeleton) {
+      return (
+        <>
+          <Skeleton />
+          <Skeleton width="w-4/5" />
+          <Skeleton width="w-1/2" />
+        </>
+      );
+    }
+
+    return (
+      <>
+        <h2 className="card-title">Property</h2>
+        <p>Property Description</p>
+      </>
+    );
+  };
+
   return (
     <div className="card w-96 shadow-xl">
       <figure className="flex flex-center bg-base-200 h-72 relative text-">
@@ -14,11 +34,11 @@ export default function PropertyCard({ propertyData, skeleton }: any) {
         )}
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Property</h2>
-        <Skeleton />
-        <p>Property Description</p>
+        {renderBody()}
         <div className="card-actions justify-end">
-          <button className="btn btn-accent">View</button>
+          <button className="btn btn-accent" disabled={skeleton}>
+            {skeleton ? <span className="loading loading-spinner"></span> : 'View'}
+          </button>
         </div>
       </div>
     </div>
