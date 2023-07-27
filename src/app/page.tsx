7d16d1c6946cabc61fc39b4e2014faa0fc7e0c2d
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-07-27 02:12:13 +0100

    Fixed pathing for sort on bedrooms and bathrooms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { Roboto_Serif } from "next/font/google";
 import PropertyAdd from "@/components/PropertyAdd";
 import PropertiesContext from "@/PropertiesContext";
 import PropertyCard from "@/components/PropertyCard";
+import { convertCurrencyToNumber } from "@/utils";
 
 const robotoSerif = Roboto_Serif({ subsets: ["latin"] });
 
@@ -12,6 +13,12 @@ export default function Home() {
   const [propertyUrls, setPropertyUrls] = useState<string[]>([]);
   const [propertyData, setPropertyData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  function handleSelectChange(event: any): void {
+    console.log(event, event.target.value, "event.target.value");
+    setSelectedOption(event.target.value);
+  }
 
   useEffect(() => {
     async function makeAPICall(url: string) {
@@ -51,6 +58,44 @@ export default function Home() {
     }
   }, [propertyUrls]);
 
+  const sortedPropertyData = useMemo(() => {
+    console.log(selectedOption, "selectedOption");
+    if (!selectedOption) return propertyData;
+
+    const sortedData = [...propertyData];
+    sortedData.sort((a: any, b: any): any => {
+      switch (selectedOption) {
+        case "Price":
+          return (
+            convertCurrencyToNumber(a.price) - convertCurrencyToNumber(b.price)
+          );
+        case "Bedrooms":
+          return (
+            +a.property?.propertyInfo?.bedrooms -
+            +b.property?.propertyInfo?.bedrooms
+          );
+        case "Bathrooms":
+          return (
+            +a.property?.propertyInfo?.bathrooms -
+            +b.property?.propertyInfo?.bathrooms
+          );
+        case "Insurance rating area":
+          return a.insuranceRatingArea - b.insuranceRatingArea;
+        case "Crimes":
+          return a.crimes - b.crimes;
+        case "Stop & searches":
+          return a.stopAndSearches - b.stopAndSearches;
+        case "Restaurants":
+          return a.restaurants - b.restaurants;
+        default:
+          return 0;
+      }
+    });
+
+    console.log(sortedData, "sortedData");
+    return sortedData;
+  }, [propertyData, selectedOption]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 gap-8">
       <h1 className="text-6xl font-bold tracking-tight">
@@ -64,16 +109,26 @@ export default function Home() {
           <PropertyAdd loading={loading} />
           <div className="flex flex-row h-fit self-end gap-4">
             <select
+              disabled={propertyUrls.length === 0 || loading}
               className="select select-accent disabled:opacity-50"
-              disabled
+              onChange={handleSelectChange}
             >
+              <option disabled selected hidden>
+                Sort by
+              </option>
               <option>Price</option>
               <option>Bedrooms</option>
               <option>Bathrooms</option>
+              <option>Insurance rating area</option>
+              <option>Crimes</option>
+              <option>Stop & searches</option>
+              <option>Restaurants</option>
             </select>
             <select
               className="select select-accent disabled:opacity-50"
-              disabled
+              disabled={
+                (propertyUrls.length === 0 || loading) && !selectedOption
+              }
             >
               <option>Ascending</option>
               <option>Descending</option>
@@ -83,7 +138,7 @@ export default function Home() {
 
         {propertyData.length > 0 && (
           <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-            {propertyData.map((property: any, index: number) => (
+            {sortedPropertyData.map((property: any, index: number) => (
               <PropertyCard
                 key={index}
                 propertyData={property}
