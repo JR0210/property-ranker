aba6a275c1d3476b213f406a5cc21418104f8a9d
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-08-02 21:40:29 +0100

    Changed prop grid col breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -146,7 +146,7 @@ export default function Home() {
         </div>
 
         {propertyData.length > 0 && (
-          <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+          <div className="w-full grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
             {sortedPropertyData.map((property: any, index: number) => (
               <PropertyCard
                 key={index}
