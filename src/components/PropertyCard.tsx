91a6c8c960484097408f15bcedf773b3c33dccad
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-08-09 00:42:52 +0100

    feat: Changed PropertyCard to use client

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useState, useRef, useContext } from "react";
+import CrimeModal from "./CrimeModal";
+import PropertiesContext from "@/utils/PropertiesContext";
+
 import Image from "next/image";
 import Skeleton from "./Skeleton";
 import { removePostcode, getPropertyTypeIcon } from "@/utils";
@@ -16,7 +22,15 @@ interface PropertyCardProps {
 
 const iconFill = "fill-neutral dark:fill-neutral-content";
 
-export default function PropertyCard({ propertyData, skeleton, breakpoint }: PropertyCardProps) {
+export default function PropertyCard({
+  propertyData,
+  skeleton,
+  breakpoint,
+}: PropertyCardProps) {
+  const { setPropertyUrls } = useContext(PropertiesContext);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   const {
     property = {},
     crime = [],
@@ -24,13 +38,10 @@ export default function PropertyCard({ propertyData, skeleton, breakpoint }: Pro
     restaurants = {},
     url,
   } = propertyData;
-  const {
-    displayImage = "",
-    address = {},
-    propertyInfo = {},
-  } = property;
+  const { displayImage = "", address = {}, propertyInfo = {} } = property;
   const PropertyIcon = getPropertyTypeIcon(propertyInfo.propertyType);
   const iconSize = breakpoint === "sm" ? 24 : 32;
+  const modalTitle = `${address.postcode?.outcode} ${address.postcode?.incode} Crime`;
 
   const renderPropertyInfoTop = () => (
     <div className="flex flex-row flex-start gap-4 mb-2">
@@ -45,11 +56,15 @@ export default function PropertyCard({ propertyData, skeleton, breakpoint }: Pro
       <div className="w-0.5 h-full bg-neutral dark:bg-neutral-content" />
       <div className="flex flex-row justify-start items-center gap-2">
         <BedroomIcon fill={iconFill} size={iconSize} />
-        <span className="text-lg md:text-xl font-bold">x{propertyInfo.bedrooms}</span>
+        <span className="text-lg md:text-xl font-bold">
+          x{propertyInfo.bedrooms}
+        </span>
       </div>
       <div className="flex flex-row justify-start items-center gap-2">
         <BathroomIcon fill={iconFill} size={iconSize} />
-        <span className="text-lg md:text-xl font-bold">x{propertyInfo.bathrooms}</span>
+        <span className="text-lg md:text-xl font-bold">
+          x{propertyInfo.bathrooms}
+        </span>
       </div>
     </div>
   );
@@ -63,7 +78,11 @@ export default function PropertyCard({ propertyData, skeleton, breakpoint }: Pro
         href={`https://www.google.com/maps/@${address.location?.latitude},${address.location?.longitude},17z`}
         target="_blank"
       >
-        <LocationPinIcon fill={iconFill} size={breakpoint === 'sm' ? 16 : 24} classNames="min-w-[16px] md:min-w-[24px]" />
+        <LocationPinIcon
+          fill={iconFill}
+          size={breakpoint === "sm" ? 16 : 24}
+          classNames="min-w-[16px] md:min-w-[24px]"
+        />
         <h2 className="card-title text-lg md:text-xl">
           {removePostcode(address.road)}, {address.postcode?.outcode}{" "}
           {address.postcode?.incode}
@@ -97,7 +116,10 @@ export default function PropertyCard({ propertyData, skeleton, breakpoint }: Pro
             </span>
           </div>
 
-          <div className="flex flex-col items-start">
+          <div
+            className="flex flex-col items-start"
+            onClick={() => setModalOpen(true)}
+          >
             <CrimeIcon fill={iconFill} size={24} />
             <span>
               No. of crimes in area: <b>{crime.length}</b>
@@ -127,37 +149,47 @@ export default function PropertyCard({ propertyData, skeleton, breakpoint }: Pro
   };
 
   return (
-    <div className="card w-full shadow-xl bg-base-300 border border-neutral-content dark:border-neutral">
-      <figure className="flex flex-center bg-base-200 h-80 relative">
-        {skeleton ? (
-          <div className="loading loading-lg"></div>
-        ) : (
-          <Image
-            src={displayImage}
-            alt={address.road}
-            style={{ objectFit: "cover" }}
-            fill
-          />
-        )}
-      </figure>
-      <div className="card-body pt-6">
-        {renderBody()}
-        <div className="card-actions justify-end mt-auto">
-          <a
-            className={`btn btn-accent ${
-              skeleton && "btn-disabled cursor-not-allowed pointer-events-none"
-            }`}
-            href={url}
-            target="_blank"
-          >
-            {skeleton ? (
-              <span className="loading loading-spinner"></span>
-            ) : (
-              "Open listing"
-            )}
-          </a>
+    <>
+      <CrimeModal
+        title={modalTitle}
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        data={crime}
+        ref={modalRef}
+      />
+      <div className="card w-full shadow-xl bg-base-300 border border-neutral-content dark:border-neutral">
+        <figure className="flex flex-center bg-base-200 h-80 relative">
+          {skeleton ? (
+            <div className="loading loading-lg"></div>
+          ) : (
+            <Image
+              src={displayImage}
+              alt={address.road}
+              style={{ objectFit: "cover" }}
+              fill
+            />
+          )}
+        </figure>
+        <div className="card-body pt-6">
+          {renderBody()}
+          <div className="card-actions justify-end mt-auto">
+            <a
+              className={`btn btn-accent ${
+                skeleton &&
+                "btn-disabled cursor-not-allowed pointer-events-none"
+              }`}
+              href={url}
+              target="_blank"
+            >
+              {skeleton ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Open listing"
+              )}
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
