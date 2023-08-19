b65b18f8771fb9b9f3c2963557ffb5e13a538a3c
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-08-20 00:18:10 +0100

    feat: Added burger JSX

--- src/components/SortInputs.tsx
+++ src/components/SortInputs.tsx
@@ -4,6 +4,7 @@ interface SortInputsProps {
   handleSelectChange: (event: any) => void;
   selectedOption: string;
   handleOrderChange: (event: any) => void;
+  type?: string;
 }
 
 export default function SortInputs({
@@ -12,7 +13,52 @@ export default function SortInputs({
   handleSelectChange,
   selectedOption,
   handleOrderChange,
+  type,
 }: SortInputsProps) {
+  if (type === "burger") {
+    return (
+      <div className="flex flex-col gap-8 w-full px-12 py-24">
+        <select
+          className="select select-accent disabled:opacity-50"
+          disabled={loading}
+          defaultValue="Crime Radius"
+        >
+          <option disabled hidden>
+            Crime Radius
+          </option>
+          <option value="0.25">1/4 Mile</option>
+          <option value="0.5">1/2 Mile</option>
+          <option value="0.75">3/4 Mile</option>
+          <option value="1">1 Mile</option>
+        </select>
+        <select
+          disabled={propertyUrls.length === 0 || loading}
+          className="select select-accent disabled:opacity-50"
+          onChange={handleSelectChange}
+          defaultValue="Sort by"
+        >
+          <option disabled hidden>
+            Sort by
+          </option>
+          <option>Price</option>
+          <option>Bedrooms</option>
+          <option>Bathrooms</option>
+          <option>Insurance rating area</option>
+          <option>Crimes</option>
+          <option>Stop & searches</option>
+          <option>Restaurants</option>
+        </select>
+        <select
+          className="select select-accent disabled:opacity-50"
+          disabled={(propertyUrls.length === 0 || loading) && !selectedOption}
+          onChange={handleOrderChange}
+        >
+          <option>Ascending</option>
+          <option>Descending</option>
+        </select>
+      </div>
+    );
+  }
   return (
     <div className="hidden lg:flex flex-col h-fit self-end gap-4">
       <select
